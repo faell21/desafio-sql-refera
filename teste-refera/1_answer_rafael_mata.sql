fd89753047861f9c4730806e98f0c9b7e28f0ca8
Rafael Cristiano <102825532+faell21@users.noreply.github.com>
2022-12-08 09:54:48 -0300

    Delete teste-refera directory

--- teste-refera/1_answer_rafael_mata.sql
+++
@@ -1,8 +0,0 @@
-SELECT fl.title AS titulo_filme,SUM(COUNT(iv.film_id)) 
-OVER(PARTITION BY iv.film_id) AS quantidade 
-FROM inventory AS iv
-    INNER JOIN rental AS rt ON rt.inventory_id=iv.inventory_id
-    INNER JOIN film AS fl ON fl.film_id=iv.film_id
-GROUP BY iv.film_id,titulo_filme
-ORDER BY quantidade DESC
-LIMIT 2;
